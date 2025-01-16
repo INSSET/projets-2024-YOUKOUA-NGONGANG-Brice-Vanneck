80a9f1d19288a77a9709084f06b9a7febd15c8c0
brice ngongang <bricengongang89@gmail.com>
2025-01-16 13:15:53 +0100

    njh

--- back_end/src/Controller/FallbackController.php
+++ back_end/src/Controller/FallbackController.php
@@ -10,7 +10,7 @@ class FallbackController
 
 
 
-//    #[Route('/{route}', name: 'fallback', requirements: ['route' => '^(?!api/).*'])]
+   #[Route('/{route}', name: 'fallback', requirements: ['route' => '^(?!api/).*'])]
     public function index(): Response
     {
         $filePath = '../public/index.html';
@@ -24,4 +24,37 @@ class FallbackController
         ]);
     }
 
-}
+
+    #[Route('/assets/{path}', name: 'serve_assets', requirements: ['path' => '.+'])]
+    public function serveAsset(string $path, Request $request): Response
+    {
+        $filePath = __DIR__ . '/../../public/assets/' . $path;
+
+        if (!file_exists($filePath)) {
+            throw $this->createNotFoundException('File not found: ' . $path);
+        }
+
+        // Déduire le type de contenu en fonction de l'extension du fichier
+        $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+        $mimeTypes = [
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'jpg' => 'image/jpeg',
+            'png' => 'image/png',
+            'gif' => 'image/gif',
+            'svg' => 'image/svg+xml',
+            'woff' => 'font/woff',
+            'woff2' => 'font/woff2',
+            'ttf' => 'font/ttf',
+            'eot' => 'application/vnd.ms-fontobject',
+        ];
+        $contentType = $mimeTypes[$extension] ?? 'application/octet-stream';
+
+        // Créer une réponse avec le contenu du fichier
+        return new Response(file_get_contents($filePath), 200, ['Content-Type' => $contentType]);
+
+    }
+
+
+
+    }
